3faaf0dc5f32e3ab488e3f4470ea593bbd5c6c62
Rick-TIY <rick@theironyard.com>
2016-08-08 10:09:42 -0600

    added some unused controller code as an example for the next assignment.

--- src/main/java/springBootApp/controllers/JspController.java
+++ src/main/java/springBootApp/controllers/JspController.java
@@ -1,6 +1,7 @@
 package springBootApp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,6 +14,14 @@ public class JspController {
 
     @RequestMapping(value="/jspTest")
     public String jspTest() {
+        return "test";
+    }
+
+    @RequestMapping(value="/getSentence")
+    public String getSentence(String s, ModelMap map) {
+        char[] c = s.toCharArray();
+        int countChars = c.length;
+        map.addAttribute("countChars",countChars);
         return "test";
     }
 }
